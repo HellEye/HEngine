d6de2fcc0ceb5b8c4b202d4a0802becb738b5c38
HellEye <thehelleye@gmail.com>
2018-10-10 18:55:55 +0200

    Bricks v2
    Added missing map png file.
    Also cleaned up hitbox code a little

--- src/com/helleye/game/objects/Hitbox.java
+++ src/com/helleye/game/objects/Hitbox.java
@@ -49,31 +49,26 @@ public class Hitbox {
 	}
 	
 	
-	//TODO Maybe check only the side that could collide (ex if facing up only check down hitbox)
 	public boolean isInHitbox(int x, int y, int width, int height, Facing facing, int fromTop, int fromBot, int fromLeft, int fromRight) {
 		if (facing == Facing.UP) {
 			for(int i=fromLeft;i<width-fromRight;i++)
 				if (isInHitbox(x + i, y) || isInHitbox(x + i, y + height)) return true;
 			for(int i=fromTop;i<height-fromBot;i++)
 				if(isInHitbox(x, y+i )||isInHitbox(x+width, y+i)) return true;
-			//return isInHitbox(x + fromRight, y + fromTop) || isInHitbox(x + width - fromLeft, y + fromTop) || isInHitbox(x + fromRight, y + height - fromBot) || isInHitbox(x + width - fromLeft, y + height - fromBot) || isInHitbox(x + width / 2, y + fromTop) || isInHitbox(x + width / 2, y + height - fromBot) || isInHitbox(x + width - fromLeft, y + height / 2) || isInHitbox(x + fromRight, y + height / 2);
 		}
 		else if (facing == Facing.DOWN) {
-			//return isInHitbox(x + fromLeft, y + fromBot) || isInHitbox(x + width - fromRight, y + fromBot) || isInHitbox(x + fromLeft, y + height - fromTop) || isInHitbox(x + width - fromRight, y + height - fromTop) || isInHitbox(x + width / 2, y + fromBot) || isInHitbox(x + width / 2, y + height - fromTop) || isInHitbox(x + width - fromRight, y + height / 2) || isInHitbox(x + fromLeft, y + height / 2);
 			for(int i=fromRight;i<width-fromLeft;i++)
 				if (isInHitbox(x + i, y) || isInHitbox(x + i, y + height)) return true;
 			for(int i=fromBot;i<height-fromTop;i++)
 				if(isInHitbox(x, y+i )||isInHitbox(x+width, y+i)) return true;
 		}
 		else if (facing == Facing.RIGHT) {
-			//return isInHitbox(x + fromBot, y + fromLeft) || isInHitbox(x + height - fromTop, y + fromLeft) || isInHitbox(x + fromBot, y + width - fromRight) || isInHitbox(x + height - fromTop, y + width - fromRight) || isInHitbox(x + height / 2, y + fromLeft) || isInHitbox(x + height / 2, y + width - fromRight) || isInHitbox(x + height - fromTop, y + width / 2) || isInHitbox(x + fromBot, y + width / 2);
 			for(int i=fromBot;i<width-fromTop;i++)
 				if (isInHitbox(x + i, y) || isInHitbox(x + i, y + height)) return true;
 			for(int i=fromLeft;i<height-fromRight;i++)
 				if(isInHitbox(x, y+i )||isInHitbox(x+width, y+i)) return true;
 		}
 		else if (facing == Facing.LEFT) {
-			//return isInHitbox(x + fromTop, y + fromRight) || isInHitbox(x + height - fromBot, y + fromRight) || isInHitbox(x + fromTop, y + width - fromLeft) || isInHitbox(x + height - fromBot, y + width - fromLeft) || isInHitbox(x + height / 2, y + fromRight) || isInHitbox(x + height / 2, y + width - fromLeft) || isInHitbox(x + height - fromBot, y + width / 2) || isInHitbox(x + fromTop, y + width / 2);
 			for(int i=fromTop;i<width-fromBot;i++)
 				if (isInHitbox(x + i, y) || isInHitbox(x + i, y + height)) return true;
 			for(int i=fromRight;i<height-fromLeft;i++)
